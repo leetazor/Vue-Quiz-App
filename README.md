4a5b0bc1691549bf4a5e4ffb00ca2fc6c51d1f38
Leetazor <zorleeta@gmail.com>
2022-07-03 19:55:57 +1000

    initial upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Vue-Quiz-App
-Just a simple quiz app made with Vue
+# quiz
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/QuizQuestions.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="questions-ctr">
+  <div class="progress">
+      <div class="bar" :style="{ width: `${ questionsAnswered / questions.length * 100}%`  } "></div>
+      <div class="status">{{ questionsAnswered }} out of {{ questions.length }} questions answered</div>
+  </div>
+
+<TransitionGroup name="fade">
+  <div class="single-question"
+  v-for="(question, index) in questions"
+  :key="question.q"
+  v-show="questionsAnswered === index"
+  >
+    <div class="question">{{ question.q }}</div>
+    <div class="answers">
+      <div
+        class="answer"
+        v-for="answer in question.answers"
+        :key="answer.text"
+        @click.prevent="selectAnswer(answer.is_correct)"
+      >
+        {{answer.text}}
+      </div>
+    </div>
+  </div>
+</TransitionGroup>  
+
+</div>
+</template>
+
+<script>
+export default {
+  props: [
+    'questions',
+    'questionsAnswered'
+  ],
+  emits:['question_answered'],
+  methods: {
+    selectAnswer(is_correct) {
+      this.$emit('question_answered', is_correct);
+    }
+  }
+}
+
+
+</script>
+
+<style>
+
+</style>
